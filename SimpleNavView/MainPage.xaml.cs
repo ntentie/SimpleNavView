f1740f03849aa04a010bff8d81a34e794096fc62
diovani ntentie <ntentiediovani@outlook.fr>
2021-12-20 11:26:05 +0100

    Add project files.

---
+++ SimpleNavView/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SimpleNavView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void NavigationView_laoded(object sender,RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(page1));
+        }
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            //  first ckeck the selected item is sittings
+            if(args.IsSettingsSelected)
+            {
+                //if your have settings page,code here
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag.ToString())
+                {
+                    case "page1":
+                        ContentFrame.Navigate(typeof(page1));
+                        break;
+                    case "page2":
+                        ContentFrame.Navigate(typeof(page2));
+                        break;
+                }
+            }
+        }
+    }
+}
